d27523e7a007210e0441c6417e00ece057012090
fransterling <roi@weareundeniable.com>
2026-01-20 18:08:04 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/backtaxes" element={<LandingPage />} />
+        <Route path="/backtaxes" element={<LandingPage customAttention="Taxpayers & Business Owners Owing $10,000+ In Back Taxes:" />} />
         <Route path="/oic" element={<LandingPage customHeadline="See If You Qualify To Settle Your Tax Debt For Less Using An Offer In Compromise" customAttention="Owe The IRS $10,000+?" />} />
         <Route path="/prequalified" element={<PreQualified />} />
       </Routes>
